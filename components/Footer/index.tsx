385dfbfb9aa2f29c60c5d3b76426127f7ac92187
Daria Diachkova <daria.diachkova@mail.ru>
2021-01-23 00:08:22 +0300

    Add components

---
+++ components/Footer/index.tsx
@@ -0,0 +1,19 @@
+import { Navigation } from "../Navigation";
+
+interface Props {
+  className?: string;
+  children?: React.ReactNode;
+}
+
+export const Footer: React.FC<Props> = ({ className, children }) => {
+  const footerClass = className || "footer";
+
+  return (
+    <footer className={footerClass}>
+      <div className="footer-content">
+        <Navigation />
+        {children}
+      </div>
+    </footer>
+  );
+};
